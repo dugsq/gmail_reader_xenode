4929a0d281bb5399676615bd8d12d63578ea9014
Vance Pan <vancep@nodally.com>
2013-11-27 18:11:08 -0800

    Resolved formatting issues in MD

--- lib/gmail_reader_xenode.rb
+++ lib/gmail_reader_xenode.rb
@@ -10,25 +10,25 @@
 # within the read email, where the output message data contains the content of the attachment.
 #
 # Configuration File Options:
-# loop_delay: defines number of seconds the Xenode waits before running the Xenode process. Expects a float. 
-# enabled: determines if the Xenode process is allowed to run. Expects true/false.
-# debug: enables extra debug messages in the log file. Expects true/false.
-# user_name: defines your gmail username. Expects a string.
-# password: defines your gmail application access token / password. Expects a string.
-# sender: defines the sender of the email to monitor. Expects a string.
+#   loop_delay: defines number of seconds the Xenode waits before running the Xenode process. Expects a float. 
+#   enabled: determines if the Xenode process is allowed to run. Expects true/false.
+#   debug: enables extra debug messages in the log file. Expects true/false.
+#   user_name: defines your gmail username. Expects a string.
+#   password: defines your gmail application access token / password. Expects a string.
+#   sender: defines the sender of the email to monitor. Expects a string.
 #
 # Example Configuration File:
-# enabled: false
-# loop_delay: 30
-# debug: false
-# user_name: "jsmith@gmaildotcom"
-# password: "abcdef123456"
-# sender: "jdoe@youremaildomaindotcom"
+#   enabled: false
+#   loop_delay: 30
+#   debug: false
+#   user_name: "jsmith@gmaildotcom"
+#   password: "abcdef123456"
+#   sender: "jdoe@youremaildomaindotcom"
 #
 # Example Input: The Gmail Reader Xenode does not expect nor handle any input. 
 #
 # Example Output:
-# msg.data:  "This string contains the actual content of the email attachment."
+#   msg.data:  "This string contains the actual content of the email attachment."
 #
 
 require 'gmail'
